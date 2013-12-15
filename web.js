13492253b794ce673d4395cb59cecb9e3dba709f
David Ewald <ewadav@uw.edu>
2013-12-14 16:36:09 -0800

    Added Angular

--- web.js
+++ web.js
@@ -7,12 +7,7 @@ var fs = require('fs');
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html', 'utf8'));
 });
-app.get('/about', function(request, response) {
-	response.send(fs.readFileSync('about.html', 'utf8'));
-});
-app.get('/contact', function(request, response) {
-	response.send(fs.readFileSync('contact.html', 'utf8'));
-});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
